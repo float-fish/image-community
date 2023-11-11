c0606be18e87291b76122d942693b2890d0ed51f
floatfish <923557344@qq.com>
2023-11-11 23:31:16 +0800

    init register

---
+++ 后端代码/denoising_picture/init.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+import random
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ysj500236@localhost:3306/denoised_picture_community"
+app.config.update(
+    MAIL_SERVER="smtp.qq.com",
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME="923557344@qq.com",
+    MAIL_PASSWORD="wuqbwtzigllfbfbg"
+)
+
+db = SQLAlchemy(app)
+
+mail = Mail(app)
+
+
+def send_mail(receiver):
+    msg = Message("Your verification code", sender="923557344@qq.com", recipients=[receiver])
+    code = random.randint(100000, 999999)
+    msg.body = 'Your verification code is:\n'+str(code)+"\nPlease don't share this code with anyone."
+    mail.send(msg)
+    return code
+
